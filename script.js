a2444207e4822d9afd2b01292b3f8f75d3a87e89
asjfhsad <113614749+asjfhsad@users.noreply.github.com>
2022-09-28 23:56:14 +0900

    indexdk

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-window.onload = function(){
-    var seconds = 00;
-    var tens = 00;
-    var OutputSeconds = document.getElementById("second");
-    var OutputTens = document.getElementById("tens");
-    var buttonStart = document.getElementById("btn-start");
-    var buttonStop = document.getElementById("btn-stop");
-    var buttonReset = document.getElementById("btn-reset");
-    var Interval;
-
-    buttonStart.addEventListener('click', () => {
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
-    });
-
-    buttonStop.addEventListener('click', () => {
-        clearInterval(Interval);
-    });
-
-    buttonReset.addEventListener('click', () => {
-        clearInterval(Interval);
-        tens = "00";
-        seconds = "00";
-        OutputSeconds.innerHTML = seconds;
-        OutputTens.innerHTML = tens;
-    });
-
-    function startTimer(){
-        tens++;
-        if(tens <= 9){
-            OutputTens.innerHTML = "0" + tens;
-        }
-
-        if(tens > 9){
-            OutputTens.innerHTML = tens;
-        }
-
-        if(tens > 99){
-            seconds++;
-            OutputSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            OutputTens.innerHTML = "0" + 0;
-        }
-
-        if(seconds > 9){
-            OutputSeconds.innerHTML = seconds;
-        }
+var pomodoro = {
+  started : false,
+  minutes : 0,
+  seconds : 0,
+  fillerHeight : 0,
+  fillerIncrement : 0,
+  interval : null,
+  minutesDom : null,
+  secondsDom : null,
+  fillerDom : null,
+  init : function(){
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.interval = setInterval(function(){
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function(){
+      self.startWork.apply(self);
+    };
+    document.querySelector('#shortBreak').onclick = function(){
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector('#longBreak').onclick = function(){
+      self.startLongBreak.apply(self);
+    };
+    document.querySelector('#stop').onclick = function(){
+      self.stopTimer.apply(self);
+    };
+  },
+  resetVariables : function(mins, secs, started){
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.fillerIncrement = 200/(this.minutes*60);
+    this.fillerHeight = 0;  
+  },
+  startWork: function() {
+    this.resetVariables(25, 0, true);
+  },
+  startShortBreak : function(){
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak : function(){
+    this.resetVariables(15, 0, true);
+  },
+  stopTimer : function(){
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit : function(num){
+    if(num < 10) {
+      return "0" + parseInt(num, 10);
     }
-}
+    return num;
+  },
+  updateDom : function(){
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+    this.fillerDom.style.height = this.fillerHeight + 'px';
+  },
+  intervalCallback : function(){
+    if(!this.started) return false;
+    if(this.seconds == 0) {
+      if(this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete : function(){
+    this.started = false;
+    this.fillerHeight = 0;
+  }
+};
+window.onload = function(){
+pomodoro.init();
+};
